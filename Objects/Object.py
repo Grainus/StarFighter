52ca73d2600d91e7684c37c791794195c8fe0629
Grainus <98911989+Grainus@users.noreply.github.com>
2022-12-09 17:46:04 -0500

    Merge branch 'Model' into Dev
    This reverts commit ce1a1741c7034f1d670646bf79c371696283387c.

--- Objects/Object.py
+++ Objects/Object.py
@@ -17,9 +17,65 @@
 # DE TOUT DOMMAGE, RÉCLAMATION OU AUTRE RESPONSABILITÉ, QUE CE SOIT DANS LE CADRE D’UN CONTRAT,
 # D’UN DÉLIT OU AUTRE, EN PROVENANCE DE, CONSÉCUTIF À OU EN RELATION AVEC LE LOGICIEL OU SON UTILISATION,
 # OU AVEC D’AUTRES ÉLÉMENTS DU LOGICIEL.
+from __future__ import annotations
 from abc import ABC
 
+from Position import Vecteur, Point, Dimension2D
 
 class Object(ABC):
     """Classe abstraite représentant un objet du jeu quel qu'il soit."""
-    raise NotImplementedError
+    def __init__(self, position: Point, width: float, height: float):
+        self.dimension = Dimension2D(width, height)
+        self.position = position
+        """Centre de l'objet"""
+        self.points = self.dimension.to_points(self.position, True)  # TODO: remove duplicate code (maybe remove annotation)
+        """Points supérieur gauche ↖ et inférieur droit ↘ de l'objeté"""
+        self.velocity = Vecteur(0, 0)
+        self.acceleration: float = 0
+
+    def _update_points(self) -> None:
+        self.points = self.dimension.to_points(self.position, True)
+
+    @property
+    def width(self):
+        return self.dimension.width
+
+    @width.setter
+    def width(self, value):
+        self.dimension.width = value
+        self._update_points()
+
+    @property
+    def height(self):
+        return self.dimension.height
+
+    @height.setter
+    def height(self, value):
+        self.dimension.height = value
+        self._update_points()
+
+    @property
+    def speed(self) -> float:
+        return self.velocity.norme
+
+    @speed.setter
+    def speed(self, value) -> None:
+        self.velocity.norme = value
+
+    def collides(self, other: Object) -> bool:
+        """Vérifie si deux objets sont en collision"""
+        overlap_x = (
+            self.points[0].x <= other.points[0].x <= self.points[1].x or
+            self.points[0].x <= other.points[1].x <= self.points[1].x
+        )
+        overlap_y = (
+            self.points[0].y <= other.points[0].y <= self.points[1].y or
+            self.points[0].y <= other.points[1].y <= self.points[1].y
+        )
+
+        return overlap_x and overlap_y
+
+    def update(self) -> None:
+        """Mise à jour de la position de l'objet selon sa vélocité"""
+        self.speed += self.acceleration
+        self.position += self.velocity
